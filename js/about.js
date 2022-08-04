86bf4ed5c2212315484836f75d5a050d5d2391e8
AlierPM <alierditmaguet@gmail.com>
2022-08-04 11:37:27 +0200

    Fix linter errors

--- js/about.js
+++ js/about.js
@@ -1,11 +1,19 @@
 const slideNavMenu = document.querySelector('.nav-menu');
 const hamburger = document.querySelector('.hamburger');
 const linkAction = document.querySelectorAll('.nav-link');
+const myButton = document.getElementById('myBtn');
 
 hamburger.addEventListener('click', () => {
   slideNavMenu.classList.toggle('slide');
   hamburger.classList.toggle('slide');
 });
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+topFunction();
 
 linkAction.forEach((n) => {
   n.addEventListener('click', () => {
@@ -16,23 +24,15 @@ linkAction.forEach((n) => {
   });
 });
 
-
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    myButton.style.display = 'block';
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = 'none';
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// When the user scrolls down 20px from the top of the document, show the button
+window.addEventListener('scroll', () => {
+  scrollFunction();
+});
--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-
 const slideNavMenu = document.querySelector('.nav-menu');
 const hamburger = document.querySelector('.hamburger');
 const linkAction = document.querySelectorAll('.nav-link');
 const speakerContainer = document.querySelector('.feat-speaker');
 const showMore = document.querySelector('#show-more');
+const myButton = document.getElementById('myBtn');
 
 hamburger.addEventListener('click', () => {
   slideNavMenu.classList.toggle('slide');
@@ -19,6 +19,12 @@ linkAction.forEach((n) => {
   });
 });
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+topFunction();
 const speakers = [
   {
     name: 'Nathaniel Bassy',
@@ -99,22 +105,15 @@ showMore.addEventListener('click', (e) => {
   }
 });
 
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    myButton.style.display = 'block';
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = 'none';
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// When the user scrolls down 20px from the top of the document, show the button
+window.addEventListener('scroll', () => {
+  scrollFunction();
+});
